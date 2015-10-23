5493e4aeeee5f4d9fa7b1b8bdf1b5103fe6e0335
Matthew Neel <matt@mrneel.com>
2015-10-23 11:00:22 -0400

    Fixed test unit.

--- test/registerServer_test.js
+++ test/registerServer_test.js
@@ -3,11 +3,11 @@ var registerServer = require("../lib/registerServer.js");
 
 
 exports.registerServerConnectionTest = function (test) {
-    var server = registerServer.create(1234);
-    var client = net.connect(1234, "127.0.0.1", function () {
+    registerServer.create(1234);
+    net.connect(1234, "127.0.0.1", function () {
         test.expect(1);
         test.ok(true, "Register client successfully connected.");
         test.done();
     });
 
-}
+};
