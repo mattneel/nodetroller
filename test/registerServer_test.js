ed4445f90d74d5a334602d539b304a87e52cb320
Matthew Neel <matt@mrneel.com>
2015-10-23 10:58:27 -0400

    Added first registerServer test.

--- test/registerServer_test.js
+++ test/registerServer_test.js
@@ -1,1 +1,13 @@
-﻿
+﻿var net = require("net");
+var registerServer = require("../lib/registerServer.js");
+
+
+exports.registerServerConnectionTest = function (test) {
+    var server = registerServer.create(1234);
+    var client = net.connect(1234, "127.0.0.1", function () {
+        test.expect(1);
+        test.ok(true, "Register client successfully connected.");
+        test.done();
+    });
+
+}
