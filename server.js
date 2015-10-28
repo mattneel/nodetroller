4ea623010f3286aca0231b4520dd688817b42733
Matthew Neel <matt@mrneel.com>
2015-10-28 15:28:17 -0400

    Data model and updating done. Working on tests and then HTTP auth & reporting.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-﻿var customerId = process.env.CUSTOMER_ID || "";
+﻿var repl = require("repl");
+var customerId = process.env.CUSTOMER_ID || "";
 var serverHost = process.env.SERVER_HOST || "localhost";
 var reportPeriod = (process.env.REPORT_PERIOD || 30) * 1000;
 var registerPort = process.env.REGISTER_PORT || 4663;
 var reportPort = process.env.REPORT_PORT || 4664;
 
-var sensors = require('./lib/sensorData.js');
 var reportServer = require('./lib/reportServer.js');
 var registerServer = require('./lib/registerServer.js');
 
-registerServer.create(registerPort, sensors);
-reportServer.create(reportPort, sensors);
+var regserv = registerServer.create(registerPort);
+var repserv = reportServer.create(reportPort);
---
+++ test/sensorData_test.js
@@ -0,0 +1,3 @@
+﻿exports.sensorDataTests = {
+
+};
