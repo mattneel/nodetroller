56f62fe55e430e7a893c47d32312d9a0f8f7a6d4
Matthew Neel <matt@mrneel.com>
2015-11-02 10:28:03 -0500

    sensorData tests complete. Moving on to HTTP auth and reporting.

--- test/sensorData_test.js
+++ test/sensorData_test.js
@@ -1,3 +1,26 @@
-﻿exports.sensorDataTests = {
+﻿var uuid = require("uuid");
+var sensorData = require("../lib/sensorData.js");
+var sampleUuid = uuid.v4().toString();
+var sampleData = 43892;
 
+exports.sensorDataTests = {
+
+    setUp: function (callback){
+        callback();
+    },
+
+    tearDown: function (callback){
+        callback();
+    },
+
+    addAndCheck: function (test)
+    {
+        sensorData.add(sampleUuid, { format: "int", length: 32, data: [] });
+        sensorData.update(sampleUuid, sampleData);
+        test.expect(1);
+        var data = sensorData.get(sampleUuid);
+        test.ok(data.data[0].data === sampleData, "sensorData working.");
+        test.done();
+
+    }
 };
