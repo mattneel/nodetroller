b7b5cef903ae9fa9f0ba2b6668520d6cb80d1a56
Matthew Neel <matt@mrneel.com>
2015-11-02 11:02:17 -0500

    HTTP auth and reporting finished. Writing small HTTP server to test against.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-﻿var repl = require("repl");
-var customerId = process.env.CUSTOMER_ID || "";
+﻿var customerId = process.env.CUSTOMER_ID || "";
 var serverHost = process.env.SERVER_HOST || "localhost";
-var reportPeriod = (process.env.REPORT_PERIOD || 30) * 1000;
+var reportPeriod = (process.env.REPORT_PERIOD || 3) * 1000;
 var registerPort = process.env.REGISTER_PORT || 4663;
 var reportPort = process.env.REPORT_PORT || 4664;
 
 var reportServer = require('./lib/reportServer.js');
 var registerServer = require('./lib/registerServer.js');
+var authenticator = require('./lib/httpAuthenticator.js');
+var reporter = require('./lib/httpReporter.js');
+
+authenticator.auth(serverHost, 80, customerId);
+reporter.report(serverHost, 80, customerId, reportPeriod);
 
 var regserv = registerServer.create(registerPort);
 var repserv = reportServer.create(reportPort);
